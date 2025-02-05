1421fc520b155f5670baa886e6ed4e053f2a3d0b
Shubham <shubhamshibu42@gmail.com>
2025-02-05 16:37:37 +0530

    user model created

---
+++ backend/models/user.model.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+const userSchema=new mongoose.Schema(
+    {
+        fullName:{
+            type:String,
+            required:true
+        },
+        email:
+        {
+            type:String,
+            required:true
+        },
+        phoneNumber:
+        {
+            type:String,
+            required:true
+        },
+        password:
+        {
+            type:String,
+            required:true
+        },
+        role:
+        {
+            type:String,
+            enum:["JobSeeker","Recruiter"],
+            default:"JobSeeker",
+            required:true
+        },
+        profile: {
+            bio:{
+                type:String
+            },
+            skills:
+                [{type:String}],
+               
+            resume:
+            {
+                type:String//URL to resume file
+            },
+            resumeOriginalName: {
+                type:String,//original name of file
+            },
+            company: {
+                type: monggose.Schema.Types.ObjectId,
+                ref: "Company"
+            },
+            profilePhoto: {
+                type:String, //URL to profile pic
+                default: "",
+            },
+
+        },
+}, {timestamps:true})
+
+export const User=mongoose.model("User",userSchema);
