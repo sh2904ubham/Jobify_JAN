1461cf5bfe977625e116baeaaaf61e4ebe6c8d8f
Shubham <shubhamshibu42@gmail.com>
2025-02-09 11:50:30 +0530

    error removal

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/componentslite/Navbar";
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
