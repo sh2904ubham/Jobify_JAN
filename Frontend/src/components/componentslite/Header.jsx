135baa8e10f6f388bdbaebe18ad83632b801839b
Shubham <shubhamshibu42@gmail.com>
2025-02-13 07:45:10 +0530

    Header Completed

--- Frontend/src/components/componentslite/Header.jsx
+++ Frontend/src/components/componentslite/Header.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Button } from "../ui/button";
+import { Search } from "lucide-react";
 
 const Header = () => {
   return (
@@ -14,6 +16,21 @@ const Header = () => {
             Find Your Perfect Job <br />
             <span className="text-violet-500">Apply & Get Hired! </span>
           </h2>
+          <p className="text-gray-600 text-sm">
+            We're here to support you in finding the perfect job that matches
+            your
+            <br /> skills and aspirations, no matter where you are.
+          </p>
+          <div className="flex w-[40%] shadow-lg border border-gray-300 pl-3 rounded-full item-centre gap-4 mx-auto">
+            <input
+              type="text"
+              placeholder="Search Jobs"
+              className="outline-none border-none w-full"
+            />
+            <Button className="rounded-r-full bg-violet-500 hover:bg-violet-600 transition-all">
+              <Search className="h-5 w-5" />
+            </Button>
+          </div>
         </div>
       </div>
     </div>
